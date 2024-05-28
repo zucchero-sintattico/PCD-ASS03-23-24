7bc92ad17e109cecb1dd42b57d2bf2d301a65d60
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-05-28 22:41:33 +0200

    feat(Environment.scala): add Point2D case class, TrafficLightState enum, TrafficLight trait, Road trait, and Environment trait with relevant methods for traffic simulation functionality

--- pt1/src/main/scala/logic/Environment.scala
+++ pt1/src/main/scala/logic/Environment.scala
@@ -1,1 +1,53 @@
-package logic 
+package logic
+
+case class Point2D(x: Double, y: Double)
+
+enum TrafficLightState:
+  case GREEN, YELLOW, RED
+
+trait TrafficLight:
+
+  def step(dt: Int): Unit
+
+  def position: Point2D
+
+  def roadPosition: Double
+
+  def isGreen: Boolean
+
+  def isRed: Boolean
+
+  def isYellow: Boolean
+
+
+
+trait Road:
+  def startPoint: Point2D
+
+  def endPoint: Point2D
+
+  def addTrafficLight(position: Point2D, initialState: TrafficLightState, greenDuration: Int, yellowDuration: Int, redDuration: Int, roadPosition: Double): Unit
+
+  def trafficLights: List[TrafficLight]
+
+  def length: Double
+
+
+trait Environment /*extends SimulationComponent*/:
+
+  def step(): Unit
+//
+//  def registerNewCarAgent(abstractCarAgent: AbstractCarAgent): Unit
+//
+//  def CarAgents(): List[AbstractCarAgent]
+
+  def createRoad(p0: Point2D, p1: Point2D): Road
+
+  def Roads(): List[Road]
+
+  def TrafficLights(): List[TrafficLight]
+//
+//  def getCurrentPerception(agentID: String): Perception
+//
+//  def doAction(agentID: String, selectedAction: Action): Unit
+
