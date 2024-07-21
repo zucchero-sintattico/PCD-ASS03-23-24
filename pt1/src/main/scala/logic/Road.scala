22a47d03e1e0616a1de5f04ae8d1d534cbb83b1e
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-07-21 12:59:56 +0200

    calculateCarPerception implemented

--- pt1/src/main/scala/logic/Road.scala
+++ pt1/src/main/scala/logic/Road.scala
@@ -12,12 +12,12 @@ object RoadActor:
   sealed trait Command
   final case class Step(dt: Int, replyTo: ActorRef[SimulationActor.RoadStepDone.type]) extends Command
   private final case class ProcessStep(dt: Int, replyTo: ActorRef[SimulationActor.RoadStepDone.type], trafficLights: Option[List[TrafficLightRecord]] = Option.empty, cars: Option[List[CarRecord]] = Option.empty, carActions: Option[List[CarAction]] = Option.empty) extends Command
-  
+
   case object TrafficLightStepDone extends Command
   final case class TrafficLightRecord(trafficLightRecord: TrafficLight) extends Command
   final case class CarRecord(carRecord: Car, carRef: ActorRef[CarActor.Command]) extends Command
   final case class CarAction(id: String, action: Action, car: ActorRef[CarActor.Command]) extends Command
-  
+
   def apply(road: Road, trafficLights: List[TrafficLight], cars: List[Car]): Behavior[Command] =
     Behaviors.setup { context =>
       val trafficLightActors = trafficLights.map(tl => context.spawn(TrafficLightActor(tl), tl.agentID))
@@ -82,7 +82,13 @@ case class RoadActor(road: Road, trafficLightActors: List[ActorRef[TrafficLightA
         case p: ProcessStep =>
           context.spawnAnonymous(
             Aggregator[CarAction, ProcessStep](
-              sendRequests = replyTo => for cars <- p.cars do cars.foreach(c => c.carRef ! CarActor.DecideAction(p.dt, Road.calculateCarPerception(c, cars), replyTo)),
+              sendRequests = replyTo =>
+                  for
+                    cars <- p.cars
+                    trafficLights <- p.trafficLights
+                    car <- cars
+                    carPerception = Road.calculateCarPerception(car, cars, trafficLights)
+                  do car.carRef ! CarActor.DecideAction(p.dt, carPerception, replyTo),
               expectedReplies = carActors.size,
               replyTo = context.self,
               aggregateReplies = replies => p.copy(carActions = Option(replies.sortBy(_.id).toList)),
@@ -105,13 +111,20 @@ case class RoadActor(road: Road, trafficLightActors: List[ActorRef[TrafficLightA
 case class RoadBuildData(road: Road, trafficLights: List[TrafficLight], cars: List[Car])
 
 object Road:
-  //is apply implicit???
-  def calculateCarPerception(car: CarRecord, cars: List[CarRecord]): CarPerception =
-    CarPerception(car.carRecord.position, findNearestCarInFront(car, cars), Option.empty)
+
+  def calculateCarPerception(car: CarRecord, cars: List[CarRecord], trafficLights: List[TrafficLightRecord]): CarPerception =
+    CarPerception(car.carRecord.position, findNearestCarInFront(car, cars), findNearestTrafficLightInFront(car, trafficLights))
 
   private def findNearestCarInFront(car: CarRecord, cars: List[CarRecord]): Option[Car] =
-    cars.map(_.carRecord).filter(_.position > car.carRecord.position).sortBy(_.position).headOption
+    cars.map(_.carRecord)
+      .filterNot(_.agentID == car.carRecord.agentID)
+      .filter(_.position > car.carRecord.position)
+      .sortBy(_.position).headOption
 
+  private def findNearestTrafficLightInFront(car: CarRecord, trafficLights: List[TrafficLightRecord]): Option[TrafficLight] =
+    trafficLights.map(_.trafficLightRecord)
+      .filter(_.trafficLightPositionInfo.roadPosition > car.carRecord.position)
+      .sortBy(_.trafficLightPositionInfo.roadPosition).headOption
 
 
 case class Road(agentID: String, startPoint: Point2D, endPoint: Point2D):
