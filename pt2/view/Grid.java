2c5f5e2a436e0007e22c7051a2ae93be959ca1cd
Alex Testa <alex.testa@studio.unibo.it>
2024-07-24 12:01:19 +0200

    Refactor

--- pt2/view/Grid.java
+++ pt2/view/Grid.java
@@ -31,29 +31,27 @@ class Grid extends JFrame{
         this.logics = new LogicsImpl();
 
         //Build grid
-        IntStream.range(0, GRID_SIZE).forEach(row -> {
-            IntStream.range(0, GRID_SIZE).forEach(column -> {
-                int number = this.logics.getNumber(row, column);
-                JTextField textField = new JTextField();
-                textField.setHorizontalAlignment(JTextField.CENTER);
-                textField.setFont(new Font("Arial", Font.BOLD, 20));
-                textField.setText(String.valueOf(number));
-                textField.setEditable(number == 0);
-                this.container.add(textField);
+        IntStream.range(0, GRID_SIZE).forEach(row -> IntStream.range(0, GRID_SIZE).forEach(column -> {
+            int number = this.logics.getNumber(row, column);
+            JTextField textField = new JTextField();
+            textField.setHorizontalAlignment(JTextField.CENTER);
+            textField.setFont(new Font("Arial", Font.BOLD, 20));
+            textField.setText(String.valueOf(number));
+            textField.setEditable(number == 0);
+            this.container.add(textField);
 
-                //Add border to sub grid
-                int top = (row % SUBGRID_SIZE == 0) ? 2 : 1;
-                int left = (column % SUBGRID_SIZE == 0) ? 2 : 1;
-                int bottom = (row == GRID_SIZE - 1) ? 2 : 1;
-                int right = (column == GRID_SIZE - 1) ? 2 : 1;
-                textField.setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, Color.BLACK));
-            });
-        });
+            //Add border to sub grid
+            int top = (row % SUBGRID_SIZE == 0) ? 2 : 1;
+            int left = (column % SUBGRID_SIZE == 0) ? 2 : 1;
+            int bottom = (row == GRID_SIZE - 1) ? 2 : 1;
+            int right = (column == GRID_SIZE - 1) ? 2 : 1;
+            textField.setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, Color.BLACK));
+        }));
 
         this.add(container);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {new Grid().setVisible(true);});
+        SwingUtilities.invokeLater(() -> new Grid().setVisible(true));
     }
 }
