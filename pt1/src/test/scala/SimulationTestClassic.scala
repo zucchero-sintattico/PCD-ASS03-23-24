a750d1f9e183615acf38ceeca6424d254cd736a3
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-08-08 11:52:15 +0200

    refactor and improve tests

---
+++ pt1/src/test/scala/SimulationTestClassic.scala
@@ -0,0 +1,43 @@
+import akka.NotUsed
+import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import logic.SimulationType.{MASSIVE_SIMULATION, CROSS_ROADS}
+import org.junit.*
+import org.junit.Assert.*
+import utils.RoadSimStatistics
+import logic.{Car, Road, SimulationHandlerActor, SimulationListener, SimulationType, TrafficLight}
+import view.ViewListenerRelayActor
+import java.util.concurrent.CompletableFuture
+
+class SimulationTestClassic:
+
+  @Test def massiveTest(): Unit =
+    val simulationDone = CompletableFuture[Unit]()
+    ActorSystem(TestSystem(simulationDone, 1, 100, MASSIVE_SIMULATION), "root")
+    simulationDone.get()
+    assertTrue(FileComparator.compareFiles("log.txt", "src/test/scala/resources/log_massive_improved.txt"))
+
+  @Test def trafficLightTest(): Unit =
+    val simulationDone = CompletableFuture[Unit]()
+    ActorSystem(TestSystem(simulationDone, 1, 500, CROSS_ROADS), "root")
+    simulationDone.get()
+    assertTrue(FileComparator.compareFiles("log.txt", "src/test/scala/resources/log_with_trafficLights_improved.txt"))
+
+  private object TestSystem:
+    def apply(simulationDone: CompletableFuture[Unit], dt: Int, step: Int, st: SimulationType): Behavior[NotUsed] =
+      Behaviors.setup { context =>
+        val controlView = new SimulationListener():
+          override def notifyInit(t: Int, agents: List[Car]): Unit = {}
+          override def notifyStepDone(t: Int, roads: List[Road], agents: List[Car], trafficLights: List[TrafficLight]): Unit = {}
+          override def notifySimulationEnded(simulationDuration: Int): Unit =
+            simulationDone.complete(())
+            context.system.terminate()
+          override def notifyStat(averageSpeed: Double): Unit = {}
+          override def simulationStopped(): Unit = {}
+        val logView = RoadSimStatistics()
+        val viewListenerRelayActor = context.spawn(ViewListenerRelayActor(List(controlView, logView)), "viewListenerRelayActor")
+        val simulationHandlerActor = context.spawn(SimulationHandlerActor(viewListenerRelayActor), "simulationHandlerActor")
+        simulationHandlerActor ! SimulationHandlerActor.SetupSimulation(st, dt, step, false)
+        simulationHandlerActor ! SimulationHandlerActor.StartSimulation
+        Behaviors.same
+      }
---
+++ pt1/src/test/scala/SimulationTestWithAkkaTest.scala
@@ -0,0 +1,41 @@
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.BeforeAndAfterAll
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.typed.scaladsl.Behaviors
+import logic.{SimulationHandlerActor, SimulationType}
+import logic.SimulationType.{CROSS_ROADS, MASSIVE_SIMULATION}
+import utils.RoadSimStatistics
+import view.ViewListenerRelayActor
+import scala.concurrent.duration.DurationInt
+
+class SimulationTestWithAkkaTest extends AnyFunSuite with Matchers with BeforeAndAfterAll:
+
+  private val testKit = ActorTestKit()
+  override def afterAll(): Unit = testKit.shutdownTestKit()
+
+  test("TrafficLight simulation result match expectation"):
+    testSimulation(CROSS_ROADS, 1, 500)
+    FileComparator.compareFiles("log.txt", "src/test/scala/resources/log_with_trafficLights_improved.txt") shouldBe true
+
+  test("Massive simulation result match expectation"):
+    testSimulation(MASSIVE_SIMULATION, 1, 5)
+    FileComparator.compareFiles("log.txt", "src/test/scala/resources/log_massive_improved.txt") shouldBe true
+
+  case object CompareResult
+  private val compareResultProbe = testKit.createTestProbe[CompareResult.type]()
+  private val logger = RoadSimStatistics()
+  private val viewListenerRelayActorMockedBehavior = Behaviors.receiveMessage[ViewListenerRelayActor.Command] {
+    case ViewListenerRelayActor.SimulationEnded(_) => compareResultProbe.ref ! CompareResult; Behaviors.same
+    case ViewListenerRelayActor.Init(t, cars) => logger.notifyInit(t, cars); Behaviors.same
+    case ViewListenerRelayActor.Stat(avgSpeed) => logger.notifyStat(avgSpeed); Behaviors.same
+    case _ => Behaviors.same
+  }
+  private val viewListenerRelayActorProbe = testKit.createTestProbe[ViewListenerRelayActor.Command]()
+  private val mockedViewListenerRelayActor = testKit.spawn(Behaviors.monitor(viewListenerRelayActorProbe.ref, viewListenerRelayActorMockedBehavior))
+
+  private def testSimulation(simulationType: SimulationType, dt: Int, step: Int): Unit =
+    val simulationHandlerActor = testKit.spawn(SimulationHandlerActor(mockedViewListenerRelayActor)) //todo handle bug in simulationHandler when start before end message concurrency
+    simulationHandlerActor ! SimulationHandlerActor.SetupSimulation(simulationType, dt, step, false)
+    simulationHandlerActor ! SimulationHandlerActor.StartSimulation
+    compareResultProbe.expectMessage(1.hours, CompareResult)
