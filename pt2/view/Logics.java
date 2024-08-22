b63a024dd217eeef2403a9815fb5049b42f8d576
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-08-22 18:45:36 +0200

    clean project

--- pt2/view/Logics.java
+++
@@ -1,9 +0,0 @@
-package pt2.view;
-
-public interface Logics {
-    boolean hit(int row, int col, int number);
-    boolean won();
-    int getNumber(int row, int col);
-    void resetGame(int emptyCells);
-}
-
