ccec8cf153c128c2f867ce00f02fbcf7e3d2f3c9
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-08-23 12:55:48 +0200

    refactor

--- pt2/SudokuRMI/src/main/java/org/src/model/user/UserDataImpl.java
+++ pt2/SudokuRMI/src/main/java/org/src/model/user/UserDataImpl.java
@@ -2,24 +2,10 @@ package org.src.model.user;
 
 import java.awt.*;
 
-public class UserDataImpl implements UserData {
-
-    private final String name;
-    private final Color color;
+public record UserDataImpl(String name, Color color) implements UserData {
 
     public UserDataImpl(String name) {
-        this.name = name;
-        this.color = new Color(name.hashCode());
-    }
-
-    @Override
-    public String name() {
-        return this.name;
-    }
-
-    @Override
-    public Color color() {
-        return this.color;
+        this(name, new Color(name.hashCode()));
     }
 
 }
