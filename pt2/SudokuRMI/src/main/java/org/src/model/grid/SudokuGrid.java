43ab325451137b3a17642e1f02785fa993658a9a
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-08-23 10:47:37 +0200

    implement input control on sudokuGrid

--- pt2/SudokuRMI/src/main/java/org/src/model/grid/SudokuGrid.java
+++ pt2/SudokuRMI/src/main/java/org/src/model/grid/SudokuGrid.java
@@ -2,7 +2,6 @@ package org.src.model.grid;
 
 import org.src.common.Point2d;
 import org.src.model.grid.cell.Cell;
-
 import java.util.List;
 
 public class SudokuGrid implements Grid {
@@ -10,23 +9,51 @@ public class SudokuGrid implements Grid {
     private final Grid sudokuGrid;
 
     public SudokuGrid(List<Cell> grid) throws IllegalArgumentException {
-        this.validateInput(grid);
+        if (!this.validateInput(grid)) {
+            throw new IllegalArgumentException("Invalid input");
+        }
         this.sudokuGrid = new GridImpl(grid);
     }
 
-    private void validateInput(List<Cell> grid) throws IllegalArgumentException {
-        //TODO implement
+    private boolean validateInput(List<Cell> grid) throws IllegalArgumentException {
+        for (int i = 0; i < SudokuFactory.GRID_SIZE; i++) {
+            for (int j = 0; j < SudokuFactory.GRID_SIZE; j++) {
+                Point2d position = new Point2d(i, j);
+                long numberOfCellsAtPosition = grid.stream().filter(cell -> cell.position().equals(position)).count();
+                if(numberOfCellsAtPosition != 1){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
     public List<Cell> cells() {
-        return sudokuGrid.cells();
+        return this.sudokuGrid.cells();
     }
 
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
+        this.buildGrid(s);
+        this.buildSelectedCellsInfo(s);
+        return s.toString();
+    }
 
+    private void buildSelectedCellsInfo(StringBuilder s) {
+        this.sudokuGrid.cells().forEach(cell -> {
+            if (cell.user().isPresent()) {
+                s.append(cell.user().get().name()).append(" -> ");
+                s.append(cell.position().x());
+                s.append(",");
+                s.append(cell.position().y());
+                s.append("\n");
+            }
+        });
+    }
+
+    private void buildGrid(StringBuilder s) {
         this.sudokuGrid.cells().forEach(cell -> {
             if (cell.number().isPresent()) {
                 s.append(cell.number().get());
@@ -37,19 +64,6 @@ public class SudokuGrid implements Grid {
             if (cell.position().y() == SudokuFactory.GRID_SIZE - 1) {
                 s.append("\n");
             }
-
         });
-        this.sudokuGrid.cells().forEach(cell -> {
-            if (cell.user().isPresent()) {
-                s.append(cell.user().get().name()).append(" -> ");
-                s.append(cell.position().x());
-                s.append(",");
-                s.append(cell.position().y());
-                s.append("\n");
-            }
-        });
-
-
-        return s.toString();
     }
 }
